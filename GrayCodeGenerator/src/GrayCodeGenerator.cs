ecfb2d2dfd95764bb4a063b8c2375c81b4de6015
David Wang <wangyudong2002@outlook.com>
2024-11-16 02:32:21 +0800

    init: init the example
    Write the gray code generator example for build.
    
    This example would generate all 65536 16-bits gray codes and store to a txt file.

---
+++ GrayCodeGenerator/src/GrayCodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿namespace GrayCodeGenerator;
+
+public static class GrayCodeGenerator
+{
+    public static List<string> GenerateGrayCodes(int bits)
+    {
+        switch (bits)
+        {
+            case 0:
+                return ["0"];
+            case 1:
+                return ["0", "1"];
+        }
+
+        // Recursive
+        var smallerGrayCodes = GenerateGrayCodes(bits - 1);
+
+        List<string> grayCodes = [];
+        grayCodes.AddRange(smallerGrayCodes.Select(grayCode => "0" + grayCode));
+        for (var i = smallerGrayCodes.Count - 1; i >= 0; i--) grayCodes.Add("1" + smallerGrayCodes[i]);
+
+        return grayCodes;
+    }
+
+    public static void SaveToFile(List<string> grayCodes, string fileName)
+    {
+        using var writer = new StreamWriter(fileName);
+        foreach (var grayCode in grayCodes) writer.WriteLine(grayCode);
+    }
+}
---
+++ GrayCodeGenerator/src/Program.cs
@@ -0,0 +1,11 @@
+﻿using static GrayCodeGenerator.GrayCodeGenerator;
+
+
+const int bits = 16;
+const string fileName = "gray_code.txt";
+
+var grayCodes = GenerateGrayCodes(bits);
+
+SaveToFile(grayCodes, fileName);
+
+Console.WriteLine($"All {grayCodes.Count} Gray codes saved to {fileName}");
